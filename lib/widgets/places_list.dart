a44a637d7c76f662b5410ae62018e6b036f91460
phumiphat <phumiphat.auk@gmail.com>
2025-06-02 21:41:03 +0700

    Implement places detail screen

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_favorite_places/models/place.dart';
+import 'package:flutter_favorite_places/screens/places_detail.dart';
 
 class PlacesList extends StatelessWidget {
   const PlacesList({super.key, required this.places});
@@ -28,6 +29,13 @@ class PlacesList extends StatelessWidget {
             color: Theme.of(context).colorScheme.onSurface,
           ),
         ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => PlaceDetailScreen(place: places[index]),
+            ),
+          );
+        },
       ),
     );
   }
